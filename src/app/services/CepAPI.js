3afe11eb208dbc5fe84ad9c758bb6323d7f904df
Rickson Lima <lima.rickson20@gmail.com>
2021-10-17 14:27:42 -0400

    updated cepAPI request

--- src/app/services/CepAPI.js
+++ src/app/services/CepAPI.js
@@ -1,14 +1,9 @@
 import axios from 'axios'
-import RequestError from '../validations/RequestError'
 
 async function requestCepAPI(cep) {
   const url = `https://ws.apicep.com/cep/${cep}.json`
 
   const response = await axios.get(url)
-
-  if (response.data.status === 400) {
-    throw new RequestError(response.data.message)
-  }
 
   return response
 }
