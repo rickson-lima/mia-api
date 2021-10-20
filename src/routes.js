0934c80fbc3818e8b258ae8228c345ed7daa0335
Rickson Lima <lima.rickson20@gmail.com>
2021-10-20 17:42:55 -0400

    feat: added new controllers functions routes

--- src/routes.js
+++ src/routes.js
@@ -6,7 +6,13 @@ import authMiddleware from './app/middlewares/auth'
 const routes = express.Router()
 
 routes.post('/api/contributor', ContributorController.create)
-routes.put('/api/contributor', ContributorController.update)
+routes.put('/api/contributor/cpf', ContributorController.storeCpf)
+routes.put('/api/contributor/email', ContributorController.storeEmail)
+routes.put('/api/contributor/telefone', ContributorController.storePhone)
+routes.put('/api/contributor/cep', ContributorController.storeCep)
+routes.put('/api/contributor/numero', ContributorController.storeNumero)
+
+// criar a rota do numero da casa do contribuinte
 
 routes.post('/api/auth', SessionController.store)
 
