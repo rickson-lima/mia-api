d5455371141a3fc8f97b54c938cf299fd1cc14ea
Rickson Lima <lima.rickson20@gmail.com>
2021-10-25 18:17:09 -0400

    fix: contributor name validation

--- src/app/validations/isValidName.js
+++ src/app/validations/isValidName.js
@@ -3,14 +3,16 @@ import RequestError from './RequestError'
 export default function isValidName(name, msg) {
   if (!name) throw new RequestError(msg)
 
-  const splittedName = name.split(' ')
-  if (splittedName.length < 2) throw new RequestError(msg)
+  const formattedName = name.split(' ').filter(String)
+  if (formattedName.length < 2) throw new RequestError(msg)
 
-  const firstName = splittedName[0]
+  const firstName = formattedName[0]
   if (firstName.length < 3) throw new RequestError(msg)
 
-  const lastName = splittedName[splittedName.length - 1]
+  const lastName = formattedName[formattedName.length - 1]
   if (lastName.length < 3) throw new RequestError(msg)
 
-  return true
+  const contributorName = formattedName.join(' ')
+
+  return contributorName.toUpperCase()
 }
