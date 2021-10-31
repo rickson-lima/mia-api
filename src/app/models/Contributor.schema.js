4685c61624c6faf9d1e680b1e28f158aeee05997
Rickson Lima <lima.rickson20@gmail.com>
2021-10-31 11:46:33 -0400

    refactor: contributor schema

--- src/app/models/Contributor.schema.js
+++ src/app/models/Contributor.schema.js
@@ -4,22 +4,31 @@ const ContributorSchema = new Schema(
   {
     cpf: {
       type: String,
-      default: null,
+      required: true,
+      unique: true,
     },
-    nome: { type: String, required: true },
 
-    status: { type: String, default: null },
+    nome: { type: String, default: null },
+
+    dataNasc: {
+      type: String,
+      required: true,
+    },
+
+    status: { percentage: String, lastIntent: Number },
 
     email: { type: String, default: null },
 
     telefone: { type: String, default: null },
 
-    cep: { type: String, default: null },
-    estado: { type: String, default: null },
-    cidade: { type: String, default: null },
-    bairro: { type: String, default: null },
-    logradouro: { type: String, default: null },
-    numero: { type: String, default: null },
+    address: {
+      cep: { type: String, default: null },
+      estado: { type: String, default: null },
+      cidade: { type: String, default: null },
+      bairro: { type: String, default: null },
+      logradouro: { type: String, default: null },
+      numero: { type: String, default: null },
+    },
   },
   { timestamps: true }
 )
